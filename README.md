0ad5bf8135ef2cc40f4e25e9b0665bcedebdc438
Ivanildo Dias <ivanildodiasdesigner@gmail.com>
2016-03-07 17:42:43 -0300

    Adapting files

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# estudio-viking-wp
-Tema WordPress do Estúdio Viking
+# Estúdio Viking WP
+Tema do Estúdio Viking para [WordPress](https://br.wordpress.org/)
+* [Página do projeto no GitHub](https://github.com/estudioviking/estudio-viking-wp)
+* [Download](https://github.com/estudioviking/estudio-viking-wp/archive/master.zip)
---
+++ functions.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Estúdio Viking WP functions and definitions
+ *
+ * Set up the theme and provides some helper functions, which are used in the
+ * theme as custom template tags. Others are attached to action and filter
+ * hooks in WordPress to change core functionality.
+ *
+ * When using a child theme you can override certain functions (those wrapped
+ * in a function_exists() call) by defining them first in your child theme's
+ * functions.php file. The child theme's functions.php file is included before
+ * the parent theme's file, so the child theme functions would be used.
+ *
+ * @link https://codex.wordpress.org/Theme_Development
+ * @link https://codex.wordpress.org/Child_Themes
+ *
+ * Functions that are not pluggable (not wrapped in function_exists()) are
+ * instead attached to a filter or action hook.
+ *
+ * For more information on hooks, actions, and filters,
+ * {@link https://codex.wordpress.org/Plugin_API}
+ *
+ * @package Estudio_Viking_WP
+ * @since 1.0.0
+ */
+
+
+/**
+ * Default theme constants
+ * ----------------------------------------------------------------------------
+ */
+define( 'ID_THEME_NAME', 'estudiovikingwp' );
+
+
+if ( ! function_exists( 'estudiovikingwp_setup' ) ) :
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ *
+ * Note that this function is hooked into the after_setup_theme hook, which
+ * runs before the init hook. The init hook is too late for some features, such
+ * as indicating support for post thumbnails.
+ *
+ * Create your own estudiovikingwp_setup() function to override in a child theme.
+ *
+ * @since 1.0.0
+ */
+function estudiovikingwp_setup() {
+	/**
+	 * Make theme available for translation.
+	 * Translations can be filed in the /languages/ directory.
+	 * If you're building a theme based on Estúdio Viking WP, change 'estudiovikingwp'
+	 * in 'ID_THEME_NAME' constant value to the name of your theme.
+	 */
+	 load_theme_textdomain( ID_THEME_NAME, get_template_directory() . '/languages' );
+
+	// Add default posts and comments RSS feed links to head.
+	add_theme_support( 'automatic-feed-links' );
+
+	/**
+	 * Let WordPress manage the document title.
+	 * By adding theme support, we declare that this theme does not use a
+	 * hard-coded <title> tag in the document head, and expect WordPress to
+	 * provide it for us.
+	 */
+	add_theme_support( 'title-tag' );
+}
+endif; // estudiovikingwp_setup
+add_action( 'after_setup_theme', 'estudiovikingwp_setup' );
+
+
+/**
+ * Sets the content width in pixels, based on the theme's design and stylesheet.
+ *
+ * Priority 0 to make it available to lower priority callbacks.
+ *
+ * @global int $content_width
+ *
+ * @since 1.0.0
+ */
+function estudiovikingwp_content_width() {
+	$GLOBALS['content_width'] = apply_filters( 'estudiovikingwp_content_width', 840 );
+}
+add_action( 'after_setup_theme', 'estudiovikingwp_content_width', 0 );
