08a574f04c2218486acf522907c079936eee20ef
Ivanildo Dias <ivanildodiasdesigner@gmail.com>
2016-05-31 18:27:57 -0300

    Update Grunt files and .gitignore

--- src/Gruntfile.js
+++ src/Gruntfile.js
@@ -13,7 +13,8 @@ module.exports = function( grunt ) {
 			css: '../assets/css',
 			img: '../assets/img',
 			js: '../assets/js',
-			sass: '../assets/sass'
+			sass: '../assets/sass',
+			temp: 'temp'
 		},
 
 		// JavaScript Linting with JSHint
@@ -101,6 +102,115 @@ module.exports = function( grunt ) {
 			options: {
 				spawn: false
 			}
+		},
+
+		// Download Dependencies
+		curl: {
+			bootstrap_sass: {
+				src: 'https://github.com/twbs/bootstrap-sass/archive/master.zip',
+				dest: '<%= dirs.temp %>/bootstrap-sass.zip'
+			},
+			woocommerce_sass: {
+				src: 'https://github.com/woothemes/woocommerce/archive/master.zip',
+				dest: '<%= dirs.temp %>/woocommerce-sass.zip'
+			}
+		},
+
+		// Unzip Files
+		unzip: {
+			bootstrap_scss: {
+				src: '<%= dirs.temp %>/bootstrap-sass.zip',
+				dest: '<%= dirs.temp %>/'
+			},
+			woocommerce_scss: {
+				src: '<%= dirs.temp %>/woocommerce-sass.zip',
+				dest: '<%= dirs.temp %>/'
+			}
+		},
+
+		// Rename and Move Directories and Files
+		rename: {
+			bootstrap_scss: {
+				src: '<%= dirs.temp %>/bootstrap-sass-master/assets/stylesheets',
+				dest: '<%= dirs.sass %>/bootstrap'
+			},
+			bootstrap_js: {
+				src: '<%= dirs.temp %>/bootstrap-sass-master/assets/javascripts/bootstrap',
+				dest: '<%= dirs.js %>/bootstrap'
+			},
+			bootstrap_fonts: {
+				src: '<%= dirs.temp %>/bootstrap-sass-master/assets/fonts/bootstrap',
+				dest: '<%= dirs.fonts %>/bootstrap'
+			},
+			woocommerce_scss: {
+				src: '<%= dirs.temp %>/woocommerce-master/assets/css',
+				dest: '<%= dirs.sass %>/woocommerce'
+			},
+			woocommerce_scss_woocommerce: {
+				src: '<%= dirs.sass %>/woocommerce/woocommerce.scss',
+				dest: '<%= dirs.sass %>/woocommerce/_woocommerce.scss'
+			},
+			woocommerce_scss_woocommerce_layout: {
+				src: '<%= dirs.sass %>/woocommerce/woocommerce-layout.scss',
+				dest: '<%= dirs.sass %>/woocommerce/_woocommerce-layout.scss'
+			},
+			woocommerce_scss_woocommerce_smallscreen: {
+				src: '<%= dirs.sass %>/woocommerce/woocommerce-smallscreen.scss',
+				dest: '<%= dirs.sass %>/woocommerce/_woocommerce-smallscreen.scss'
+			},
+			woocommerce_fonts: {
+				src: '<%= dirs.temp %>/woocommerce-master/assets/fonts',
+				dest: '<%= dirs.fonts %>/woocommerce'
+			},
+			woocommerce_images: {
+				src: '<%= dirs.temp %>/woocommerce-master/assets/images',
+				dest: '<%= dirs.img %>/woocommerce'
+			}
+		},
+
+		// Clean Directories and Files
+		clean: {
+			options: {
+				force: true
+			},
+			bootstrap_prepare: [
+				'<%= dirs.temp %>',
+				'<%= dirs.sass %>/bootstrap/',
+				'<%= dirs.js %>/bootstrap/',
+				'<%= dirs.js %>/libs/bootstrap.min.js',
+				'<%= dirs.fonts %>/bootstrap/'
+			],
+			bootstrap: [
+				'<%= dirs.temp %>'
+			],
+			woocommerce_prepare: [
+				'<%= dirs.temp %>',
+				'<%= dirs.sass %>/woocommerce/',
+				'<%= dirs.fonts %>/woocommerce/',
+				'<%= dirs.img %>/woocommerce/'
+			],
+			woocommerce: [
+				'<%= dirs.sass %>/woocommerce/{activation,admin,chosen,dashboard,menu,prettyPhoto,reports-print,select2}**',
+				'<%= dirs.sass %>/woocommerce/*.css',
+				'<%= dirs.temp %>'
+			]
+		},
+
+		replace: {
+			woocommerce: {
+				src: ['<%= dirs.sass %>/woocommerce/*.scss'],
+		 		overwrite: true,
+				replacements: [{
+					from: /@import ".+";\n/g,
+					to: ''
+				},{
+					from: '../fonts/',
+					to: '../fonts/woocommerce/'
+				},{
+					from: '../img/',
+					to: '../img/woocommerce/'
+				}]
+			}
 		}
 	};
 
@@ -112,7 +222,36 @@ module.exports = function( grunt ) {
 		'jshint',
 		'sass',
 		'uglify'
-	]);
+	] );
+
+	// Bootstrap Task
+	grunt.registerTask( 'bootstrap', [
+		'clean:bootstrap_prepare',
+		'curl:bootstrap_sass',
+		'unzip:bootstrap_scss',
+		'rename:bootstrap_scss',
+		'rename:bootstrap_js',
+		'rename:bootstrap_fonts',
+		'clean:bootstrap',
+		'uglify:bootstrap',
+		'sass'
+	] );
+
+	// Woocommerce Task
+	grunt.registerTask( 'woo', [
+		'clean:woocommerce_prepare',
+		'curl:woocommerce_sass',
+		'unzip:woocommerce_scss',
+		'rename:woocommerce_scss',
+		'rename:woocommerce_scss_woocommerce',
+		'rename:woocommerce_scss_woocommerce_layout',
+		'rename:woocommerce_scss_woocommerce_smallscreen',
+		'rename:woocommerce_fonts',
+		'rename:woocommerce_images',
+		'clean:woocommerce',
+		'replace:woocommerce',
+		'sass'
+	] );
 
 	// Short Aliases
 	grunt.registerTask( 'w', ['watch'] );
